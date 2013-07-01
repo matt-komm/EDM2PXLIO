83feabc8ab5223e7e1b8243f72aa5cbd605faacc
matt-komm <mkomm@ingrid-ui1.cism.ucl.ac.be>
2013-07-01 17:22:05 +0200

    add Electron converter

--- src/EDM2PXLIO.cc
+++ src/EDM2PXLIO.cc
@@ -42,8 +42,10 @@
 
 #include "EDM2PXLIO/EDM2PXLIO/src/Collection2Pxlio.h"
 #include "EDM2PXLIO/EDM2PXLIO/src/converters/PatMuon2Pxlio.h"
+#include "EDM2PXLIO/EDM2PXLIO/src/converters/PatElectron2Pxlio.h"
 #include "EDM2PXLIO/EDM2PXLIO/src/converters/PatJet2Pxlio.h"
 #include "EDM2PXLIO/EDM2PXLIO/src/converters/PatMET2Pxlio.h"
+
 //
 // class declaration
 //
@@ -69,8 +71,10 @@ class EDM2PXLIO : public edm::EDAnalyzer {
       pxl::OutputFile pxlFile_;
       
       PatMuon2Pxlio muonCollection_;
+      PatElectron2Pxlio electronCollection_;
       PatJet2Pxlio jetCollection_;
       PatMET2Pxlio metCollection_;
+      
       // ----------member data ---------------------------
 };
 
@@ -88,10 +92,12 @@ class EDM2PXLIO : public edm::EDAnalyzer {
 EDM2PXLIO::EDM2PXLIO(const edm::ParameterSet& iConfig):
     pxlFile_(iConfig.getUntrackedParameter<std::string>("fileName")),
     muonCollection_("muon"),
+    electronCollection_("electron"),
     jetCollection_("jet"),
     metCollection_("met")
 {
     muonCollection_.parseParameter(iConfig);
+    electronCollection_.parseParameter(iConfig);
     jetCollection_.parseParameter(iConfig);
     metCollection_.parseParameter(iConfig);
 }
@@ -119,6 +125,7 @@ EDM2PXLIO::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
     pxlEvent.setUserRecord<unsigned int>("Event number", iEvent.id().event());
     
     muonCollection_.convert(&iEvent,&pxlEvent);
+    electronCollection_.convert(&iEvent,&pxlEvent);
     jetCollection_.convert(&iEvent,&pxlEvent);
     metCollection_.convert(&iEvent,&pxlEvent);
     
---
+++ src/converters/PatElectron2Pxlio.h
@@ -0,0 +1,95 @@
+#ifndef _PATELECTRON2PXLIO_H_
+#define _PATELECTRON2PXLIO_H_
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+
+#include "DataFormats/PatCandidates/interface/Electron.h"
+
+#include "DataFormats/VertexReco/interface/Vertex.h"
+
+#include "PhysicsTools/PatUtils/interface/StringParserTools.h"
+
+#include "Pxl/Pxl/interface/Pxl.h"
+
+#include "EDM2PXLIO/EDM2PXLIO/src/Collection2Pxlio.h"
+
+#include "EDM2PXLIO/EDM2PXLIO/src/converters/Pat2Pxlio.h"
+
+#include <iostream>
+
+class PatElectron2Pxlio: public Pat2Pxlio<pat::Electron>
+{
+    protected:
+        const reco::Vertex* primaryVertex_;
+        std::string vertexInputTag_;
+    public:
+        PatElectron2Pxlio(std::string name,std::string vertexInputTag=std::string()):
+            Pat2Pxlio<pat::Electron>(name),
+            vertexInputTag_(vertexInputTag)
+        {
+        }
+        
+        virtual void convert(const edm::Event* edmEvent, pxl::Event* pxlEvent)
+        {
+            /*
+            if (vertexInputTag_.length()>0) {
+                edm::Handle<std::vector<reco::Vertex>> vertexList;
+                edmEvent->getByLabel(vertexInputTag_,vertexList);
+                primaryVertex_ = &(*vertexList)[0];
+            }
+            */
+            Pat2Pxlio<pat::Electron>::convert(edmEvent,pxlEvent);
+            
+        }
+                
+        virtual void convertObject(const pat::Electron& patObject, pxl::Particle* pxlParticle)
+        {
+            pxlParticle->setCharge(patObject.charge());
+            pxlParticle->setUserRecord<float>("dB",patObject.dB());
+            pxlParticle->setUserRecord<float>("mva",patObject.electronID("mvaTrigV0"));
+            pxlParticle->setUserRecord<bool>("ecalDrivenSeed",patObject.ecalDrivenSeed());
+            pxlParticle->setUserRecord<bool>("trackerDrivenSeed",patObject.trackerDrivenSeed());
+            pxlParticle->setUserRecord<bool>("isInEB-EE",(1.4442 < fabs(patObject.eta())) && (fabs(patObject.eta()) < 1.5660));
+            
+            pxlParticle->setUserRecord<bool>("passConversionVeto",patObject.passConversionVeto());
+            pxlParticle->setUserRecord<int>("numberOfHits",patObject.gsfTrack()->trackerExpectedHitsInner().numberOfHits());
+            
+            if (primaryVertex_)
+            {
+                double dz =fabs(patObject.gsfTrack()->dxy(primaryVertex_->position()));
+	            pxlParticle->setUserRecord<float>("dz",dz);
+            }
+
+        }
+        
+        virtual void convertP4(const pat::Electron& patObject, pxl::Particle* pxlParticle)
+        {
+            std::cout<<"using alternative electron momentum"<<std::endl;
+            pxlParticle->setP4(patObject.ecalDrivenMomentum().px(),patObject.ecalDrivenMomentum().py(),patObject.ecalDrivenMomentum().pz(),patObject.ecalDrivenMomentum().energy());
+        }
+        
+        virtual void convertCollection(const std::vector<pat::Electron>* patObjectList, std::vector<pxl::Particle*> pxlParticleList)
+        {
+        }
+        
+        ~PatElectron2Pxlio()
+        {
+        }
+};
+
+#endif
+
