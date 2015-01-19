3503ce9ed887dc42766be36f527eaf2f6ca9e835
Matthias <mkomm@cern.ch>
2015-01-19 19:38:07 +0100

    add missing files

---
+++ Core/plugins/EDM2PXLIO.cc
@@ -0,0 +1,241 @@
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+
+
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "Pxl/Pxl/interface/pxl/core.hh"
+#include "Pxl/Pxl/interface/pxl/hep.hh"
+
+#include "EDM2PXLIO/Core/interface/ConverterFactory.h"
+
+#include <string>
+#include <vector>
+
+class EDM2PXLIO : public edm::EDAnalyzer 
+{
+    private: 
+        struct SelectedProcessPaths
+        {
+            std::string processName;
+            std::vector<std::string> paths;
+        };
+        
+    public:
+        explicit EDM2PXLIO(const edm::ParameterSet&);
+        ~EDM2PXLIO();
+
+        static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+    private:
+        bool checkPath(const edm::Event&, pxl::Event&);
+        std::vector<SelectedProcessPaths> _selectedProcessPaths;
+        
+        pxl::OutputFile* _pxlFile;
+        std::string _processName;
+
+        std::vector<edm2pxlio::Converter*> _converters; 
+        
+        
+
+        virtual void beginJob() ;
+        virtual void analyze(const edm::Event&, const edm::EventSetup&);
+        virtual void endJob() ;
+
+        virtual void beginRun(edm::Run const&, edm::EventSetup const&);
+        virtual void endRun(edm::Run const&, edm::EventSetup const&);
+        virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+        virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+
+
+
+};
+
+
+EDM2PXLIO::EDM2PXLIO(const edm::ParameterSet& globalConfig):
+    _pxlFile(nullptr)
+{
+    if (globalConfig.exists("selectEvents"))
+    {
+        const std::vector<edm::ParameterSet>& selectEventPSets = globalConfig.getParameter<std::vector<edm::ParameterSet>>("selectEvents");
+        for (unsigned int iset=0; iset<selectEventPSets.size(); ++iset)
+        {
+            SelectedProcessPaths selectedProcessPath;
+            selectedProcessPath.processName=selectEventPSets[iset].getParameter<std::string>("process"); 
+            selectedProcessPath.paths=selectEventPSets[iset].getParameter<std::vector<std::string>>("paths");
+            _selectedProcessPaths.push_back(selectedProcessPath);
+        }
+    }
+    else
+    {
+        edm::LogWarning("no selectEvents configured") << "will store events from all processes and paths";
+    }
+    if (globalConfig.exists("outFileName")) {
+        _pxlFile = new pxl::OutputFile(globalConfig.getParameter<std::string>("outFileName"));
+    } else {
+        edm::LogWarning("no output file name configured") << "default name 'out.pxlio' will be used";
+        _pxlFile = new pxl::OutputFile("out.pxlio");
+    }
+    
+    if (globalConfig.exists("processName")) {
+        _processName = globalConfig.getParameter<std::string>("processName");
+    } else {
+        _processName = "";
+    }
+
+    edm::ConsumesCollector consumeCollect = consumesCollector();
+    
+    const std::vector<std::string> psetNames = globalConfig.getParameterNamesForType<edm::ParameterSet>();
+    for (unsigned int iname=0; iname< psetNames.size(); ++iname)
+    {
+        const std::string& converterName = psetNames[iname];
+        const edm::ParameterSet& localConf = globalConfig.getParameter<edm::ParameterSet>(converterName);
+        if (localConf.exists("type"))
+        {
+            std::string typeName = localConf.getParameter<std::string>("type");
+            edm2pxlio::Converter* converter = edm2pxlio::ConverterFactory::get()->tryToCreate(typeName,converterName,globalConfig,consumeCollect);
+            if (converter)
+            {
+                _converters.push_back(converter);
+            }
+            else
+            {
+                edm::LogWarning("Converter plugin not found: ") << typeName;
+            }
+        }
+    }
+}
+
+
+EDM2PXLIO::~EDM2PXLIO()
+{
+}
+
+void
+EDM2PXLIO::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+    
+    pxl::Event pxlEvent;
+    if (!checkPath(iEvent,pxlEvent))
+    {
+        return;
+    }
+    pxlEvent.setUserRecord("Run", iEvent.run());
+    pxlEvent.setUserRecord("Event number", (uint64_t)iEvent.id().event());
+    pxlEvent.setUserRecord("LuminosityBlock",iEvent.luminosityBlock());
+    pxlEvent.setUserRecord("isRealData",iEvent.isRealData());
+    if (_processName.length()>0)
+    {
+        pxlEvent.setUserRecord("Process", _processName);
+    }
+    
+    for (unsigned int iconverter = 0; iconverter<_converters.size(); ++iconverter)
+    {
+        _converters[iconverter]->convert(&iEvent,&iSetup,&pxlEvent);
+    }
+    
+    _pxlFile->streamObject(&pxlEvent);
+    _pxlFile->writeFileSection();
+}
+
+bool
+EDM2PXLIO::checkPath(const edm::Event& iEvent, pxl::Event& pxlEvent)
+{
+    if (_selectedProcessPaths.size()==0)
+    {   
+        return true;
+    }
+    
+    bool accept=false;
+    for (unsigned int iprocess =0; iprocess < _selectedProcessPaths.size(); ++iprocess)
+    {
+        const SelectedProcessPaths& selectedProcessPath = _selectedProcessPaths[iprocess];
+        edm::TriggerResultsByName result = iEvent.triggerResultsByName(selectedProcessPath.processName);
+        if (! result.isValid())
+        {
+            edm::LogWarning("trigger result not valid") << "event will not be processed";
+            return false;
+        }
+        const std::vector<std::string>& paths = selectedProcessPath.paths;
+        if (paths.size()==0)
+        {
+            //accept all events
+            return true;
+        }
+        for (unsigned ipath=0; ipath<paths.size();++ipath)
+        {
+            if (!result.wasrun(paths[ipath]))
+            {
+                edm::LogWarning("TriggerResults has no cms.Path named: ") << paths[ipath] << " in process '"<<selectedProcessPath.processName<<"'. The result of this path will be ignored.";
+            } else {
+                accept = accept || result.accept(paths[ipath]);
+            }
+        }
+    }
+    return accept;
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+EDM2PXLIO::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+EDM2PXLIO::endJob() 
+{
+    _pxlFile->close();
+    delete _pxlFile;
+}
+
+// ------------ method called when starting to processes a run  ------------
+void 
+EDM2PXLIO::beginRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when ending the processing of a run  ------------
+void 
+EDM2PXLIO::endRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when starting to processes a luminosity block  ------------
+void 
+EDM2PXLIO::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when ending the processing of a luminosity block  ------------
+void 
+EDM2PXLIO::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+EDM2PXLIO::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(EDM2PXLIO);
---
+++ miniAODtest.py
@@ -0,0 +1,243 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("S2")
+
+process.load("Configuration.EventContent.EventContent_cff")
+process.load('Configuration.StandardSequences.Geometry_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'POSTLS172_V3::All'
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
+process.MessageLogger.suppressWarning = cms.untracked.vstring('ecalLaserCorrFilter','manystripclus53X','toomanystripclus53X')
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+process.options.allowUnscheduled = cms.untracked.bool(False)
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        "file:test.root"
+    )
+)
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+
+
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.lessGenParticles = cms.EDProducer("GenParticlePruner",
+    src=cms.InputTag("prunedGenParticles"),
+    select=cms.vstring(
+        "drop  *",
+        "keep abs(status)>20 & abs(status) < 30", #keeps all particles from the hard matrix element
+        "keep++ abs(pdgId)=24", #keep W-boson decay
+        "drop abs(status)>30" #drop all intermediate from decay
+    )
+)
+
+
+from RecoJets.JetProducers.ak4PFJets_cfi import ak4PFJets
+from RecoJets.JetProducers.ak4GenJets_cfi import ak4GenJets
+from RecoMET.METProducers.PFMET_cfi import pfMet
+
+
+
+#select isolated collections
+process.selectedMuons = cms.EDFilter("CandPtrSelector", 
+    src = cms.InputTag("slimmedMuons"), 
+    cut = cms.string(
+        '''
+        abs(eta)<2.5 && pt>10. &&
+        (pfIsolationR04().sumChargedHadronPt+
+        max(0.,pfIsolationR04().sumNeutralHadronEt+
+        pfIsolationR04().sumPhotonEt-
+        0.50*pfIsolationR04().sumPUPt))/pt < 0.20 &&
+        (isPFMuon && (isGlobalMuon || isTrackerMuon) )
+        '''
+    )
+)
+process.selectedElectrons = cms.EDFilter("CandPtrSelector", 
+    src = cms.InputTag("slimmedElectrons"), 
+    cut = cms.string(
+        '''
+        abs(eta)<2.5 && pt>20. &&
+        gsfTrack.isAvailable() &&
+        gsfTrack.hitPattern().numberOfLostHits(\'MISSING_INNER_HITS\') < 2 &&
+        (pfIsolationVariables().sumChargedHadronPt+
+        max(0.,pfIsolationVariables().sumNeutralHadronEt+
+        pfIsolationVariables().sumPhotonEt-
+        0.5*pfIsolationVariables().sumPUPt))/pt < 0.15
+        '''
+    )
+)
+#do projections
+process.pfCHS = cms.EDFilter("CandPtrSelector", 
+    src = cms.InputTag("packedPFCandidates"), 
+    cut = cms.string("fromPV")
+)
+process.pfNoMuonCHS = cms.EDProducer("CandPtrProjector", 
+    src = cms.InputTag("pfCHS"), 
+    veto = cms.InputTag("selectedMuons")
+)
+process.pfNoElectronsCHS = cms.EDProducer("CandPtrProjector", 
+    src = cms.InputTag("pfNoMuonCHS"), 
+    veto = cms.InputTag("selectedElectrons")
+)
+
+process.ak4PFJetsCHS = ak4PFJets.clone(
+    src = 'pfNoElectronsCHS', 
+    doAreaFastjet = True
+) # no idea while doArea is false by default, but it's True in RECO so we have to set it
+
+
+
+
+# get the tracks, primary & secondary vertices from packedPFCandidates
+process.unpackedTracksAndVertices = cms.EDProducer('PATTrackAndVertexUnpacker',
+    slimmedVertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+    slimmedSecondaryVertices = cms.InputTag("slimmedSecondaryVertices"),
+    additionalTracks= cms.InputTag("lostTracks"),
+    packedCandidates = cms.InputTag("packedPFCandidates")
+)
+
+process.load('RecoBTag.Configuration.RecoBTag_cff')
+process.load('RecoJets.Configuration.RecoJetAssociations_cff')
+process.load("PhysicsTools.PatAlgos.producersLayer1.jetProducer_cff")
+
+process.patJets.addJetCharge = False #so jet charge module can be removed?
+process.patJets.addBTagInfo = True
+process.patJets.getJetMCFlavour = False
+process.patJets.addAssociatedTracks = False
+process.patJetPartonMatch.matched = "prunedGenParticles"
+process.patJetCorrFactors.primaryVertices = "offlineSlimmedPrimaryVertices"
+
+process.ak4JetTracksAssociatorAtVertexPF.jets = cms.InputTag("ak4PFJetsCHS")
+process.ak4JetTracksAssociatorAtVertexPF.tracks = cms.InputTag("unpackedTracksAndVertices")
+process.impactParameterTagInfos.primaryVertex = cms.InputTag("unpackedTracksAndVertices")
+process.pfImpactParameterTagInfos.primaryVertex = cms.InputTag("unpackedTracksAndVertices")
+process.pfImpactParameterTagInfos.candidates=cms.InputTag("packedPFCandidates") #no clue
+process.inclusiveSecondaryVertexFinderTagInfos.extSVCollection = cms.InputTag("unpackedTracksAndVertices","secondary","")
+
+
+process.patJetGenJetMatch.matched = "slimmedGenJets"
+process.patJetPartonMatch.matched = "prunedGenParticles"
+process.patJetCorrFactors.primaryVertices = "offlineSlimmedPrimaryVertices"
+
+process.patJetPartons.particles = "prunedGenParticles"
+process.patJetPartonsLegacy.src = "prunedGenParticles" # if using legacy jet flavour (not used by default)
+process.patJetPartonsLegacy.skipFirstN = cms.uint32(0) # do not skip first 6 particles, we already pruned some!
+process.patJetPartonsLegacy.acceptNoDaughters = cms.bool(True) # as we drop intermediate stuff, we need to accept quarks with no siblings
+
+
+
+#this may not make sense, slimmed object may not be soft
+process.softMuonTagInfos.primaryVertex=cms.InputTag("offlineSlimmedPrimaryVertices")
+process.softMuonTagInfos.jets=cms.InputTag("ak4PFJetsCHS")
+process.softMuonTagInfos.leptons=cms.InputTag("slimmedMuons")
+process.softPFMuonsTagInfos.primaryVertex=cms.InputTag("offlineSlimmedPrimaryVertices")
+process.softPFMuonsTagInfos.jets=cms.InputTag("ak4PFJetsCHS")
+process.softPFMuonsTagInfos.leptons=cms.InputTag("slimmedMuons")
+
+
+process.softPFElectronsTagInfos.primaryVertex=cms.InputTag("offlineSlimmedPrimaryVertices")
+process.softPFElectronsTagInfos.jets=cms.InputTag("ak4PFJetsCHS")
+process.softPFElectronsTagInfos.leptons=cms.InputTag("slimmedElectrons")
+
+    
+process.p0=cms.Path(
+    process.lessGenParticles*
+    process.pfCHS*
+    process.selectedMuons*process.pfNoMuonCHS*
+    process.selectedElectrons*process.pfNoElectronsCHS*
+    process.ak4PFJetsCHS*process.unpackedTracksAndVertices*
+    
+    #process.recoJetAssociations*
+    #only need this product
+    process.ak4JetTracksAssociatorAtVertexPF* 
+    
+    
+    
+    process.btagging* 
+    
+    
+    
+    process.makePatJets
+)
+
+
+process.OUT = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('projected.root'),
+    outputCommands = cms.untracked.vstring('keep *','keep patJets_patJetsAK4PF_*_*','keep patJets_patJetsAK4PFCHS_*_*','keep *_*_*_PAT','keep *_*_*_S2')
+)
+
+
+
+
+
+
+process.pat2pxlio=cms.EDAnalyzer('EDM2PXLIO',
+    outFileName=cms.string("projected.pxlio"),
+    process=cms.string("tChannel"),
+    selectEvents=cms.VPSet(
+        cms.PSet(
+            process=cms.string("S2"),
+            paths=cms.vstring("p0"),
+        )
+    ),
+    muons = cms.PSet(
+        type=cms.string("CandidateConverter"),
+        srcs=cms.VInputTag(cms.InputTag("slimmedMuons")),
+        names=cms.vstring("Muon")
+    )
+    
+)
+'''
+primaryVertex=cms.InputTag("offlineSlimmedPrimaryVertices"),
+
+genCollection = cms.PSet(
+    type=cms.string("GenParticle2Pxlio"),
+    srcs=cms.VInputTag(cms.InputTag("lessGenParticles")),
+    GenEventInfo=cms.InputTag('generator'),
+    LHEEvent=cms.InputTag('source')
+),
+
+jets = cms.PSet(
+    type=cms.string("PatJet2Pxlio"),
+    srcs=cms.VInputTag(cms.InputTag("slimmedJets"),cms.InputTag("patJets")),
+    names=cms.vstring("Jet","ReclusteredJet"),
+    select=cms.string("pt>10"),
+    
+    #userRecords=cms.PSet(
+    #    abseta=cms.string("abs(eta)")
+    #)
+),
+
+
+muons = cms.PSet(
+    type=cms.string("PatMuon2Pxlio"),
+    srcs=cms.VInputTag(cms.InputTag("slimmedMuons")),
+    names=cms.vstring("Muon")
+),
+
+electrons = cms.PSet(
+    type=cms.string("PatElectron2Pxlio"),
+    srcs=cms.VInputTag(cms.InputTag("slimmedElectrons")),
+    names=cms.vstring("Electron")
+),
+
+mets = cms.PSet(
+    type=cms.string("PatMET2Pxlio"),
+    srcs=cms.VInputTag(cms.InputTag("slimmedMETs")),
+    names=cms.vstring("MET")
+),
+
+triggers = cms.PSet(
+    type=cms.string("Trigger2Pxlio"),
+    srcs=cms.VInputTag(cms.InputTag("TriggerResults")),
+    regex=cms.vstring("Iso[0-9a-zA-z]*")
+)
+#TODO: add prescale info from: pat::PackedTriggerPrescales "patTrigger" 
+'''
+
+
+process.endpath= cms.EndPath(process.OUT*process.pat2pxlio)
+
+
