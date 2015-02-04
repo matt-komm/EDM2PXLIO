65c50ab74e5a60646dd5068ab63e3a6e1af4f7f3
Matthias <mkomm@cern.ch>
2015-02-04 11:20:26 +0100

    added pat electron converter

---
+++ Pat/plugins/ElectronConverter.cc
@@ -0,0 +1,90 @@
+#include "EDM2PXLIO/Pat/plugins/ElectronConverter.h"
+#include "EDM2PXLIO/Core/interface/ConverterFactory.h"
+
+namespace edm2pxlio
+{
+
+ElectronConverter::ElectronConverter(const std::string& name, const edm::ParameterSet& globalConfig, edm::ConsumesCollector& consumesCollector):
+    Base(name, globalConfig, consumesCollector)
+{
+}
+
+
+void ElectronConverter::convertObject(const pat::Electron& patObject, pxl::Particle* pxlParticle) const
+{
+    Base::convertObject(patObject, pxlParticle);
+    pxlParticle->setP4(patObject.px(),patObject.py(),patObject.pz(),patObject.energy());
+    pxlParticle->setCharge(patObject.charge());
+    pxlParticle->setUserRecord("dB",patObject.dB());
+    const std::vector<pat::Electron::IdPair>& electronIds = patObject.electronIDs();
+    for (unsigned int i = 0; i < electronIds.size(); ++i)
+    {
+        pxlParticle->setUserRecord(electronIds[i].first,(bool)electronIds[i].second);
+    }
+    
+    pxlParticle->setUserRecord("ecalDrivenSeed",patObject.ecalDrivenSeed());
+    pxlParticle->setUserRecord("trackerDrivenSeed",patObject.trackerDrivenSeed());
+    pxlParticle->setUserRecord("isInEB-EE",(1.4442 < fabs(patObject.eta())) && (fabs(patObject.eta()) < 1.5660));
+    
+    pxlParticle->setUserRecord("passConversionVeto",patObject.passConversionVeto());
+    //pxlParticle->setUserRecord("numberOfHits",patObject.gsfTrack()->trackerExpectedHitsInner().numberOfHits());
+
+    
+    if (patObject.gsfTrack().get()!=nullptr)
+    {
+        const reco::Track* gsfTrack = patObject.gsfTrack().get();
+        pxlParticle->setUserRecord("chi2",gsfTrack->chi2());
+        pxlParticle->setUserRecord("ndof",gsfTrack->ndof());
+        pxlParticle->setUserRecord("lostHits",gsfTrack->lost());
+        /*
+        if (primaryVertexProvider_->getPrimaryVertex())
+        {
+            double dz =fabs(patObject.gsfTrack()->dz(primaryVertexProvider_->getPrimaryVertex()->position()));
+            double dxy =fabs(patObject.gsfTrack()->dxy(primaryVertexProvider_->getPrimaryVertex()->position()));
+            pxlParticle->setUserRecord("dz",dz);
+            pxlParticle->setUserRecord("dxy",dxy);
+        }
+        */
+    }
+    
+    pxlParticle->setUserRecord("caloIso",patObject.caloIso());
+    pxlParticle->setUserRecord("ecalIso",patObject.ecalIso());
+    pxlParticle->setUserRecord("hcalIso",patObject.hcalIso());
+    pxlParticle->setUserRecord("trackIso",patObject.trackIso());
+
+    
+    pxlParticle->setUserRecord("R03TkSumPt",patObject.dr03TkSumPt());
+    pxlParticle->setUserRecord("R03EcalRecHitSumEt",patObject.dr03EcalRecHitSumEt());
+    pxlParticle->setUserRecord("R03HcalTowerSumEt",patObject.dr03HcalTowerSumEt());
+    
+
+    pxlParticle->setUserRecord("chargedHadronIso",patObject.chargedHadronIso());
+    pxlParticle->setUserRecord("neutralHadronIso",patObject.neutralHadronIso());
+    pxlParticle->setUserRecord("photonIso",patObject.photonIso());
+    pxlParticle->setUserRecord("superClusterEta",patObject.superCluster()->eta());
+    
+    pxlParticle->setUserRecord("sigmaIetaIeta",patObject.sigmaIetaIeta());
+    pxlParticle->setUserRecord("hadronicOverEm",patObject.hadronicOverEm());
+    pxlParticle->setUserRecord("fbrem",patObject.fbrem());
+    
+    
+
+
+    pxlParticle->setUserRecord("patPt",patObject.pt());
+    pxlParticle->setUserRecord("patPx",patObject.px());
+    pxlParticle->setUserRecord("patPy",patObject.py());
+    pxlParticle->setUserRecord("patPz",patObject.pz());
+    pxlParticle->setUserRecord("patEta",patObject.eta());
+    pxlParticle->setUserRecord("patE",patObject.energy());
+    pxlParticle->setUserRecord("patEt",patObject.et());
+}
+
+void ElectronConverter::convertP4(const pat::Electron& patObject, pxl::Particle* pxlParticle) const
+{
+    pxlParticle->setP4(patObject.ecalDrivenMomentum().px(),patObject.ecalDrivenMomentum().py(),patObject.ecalDrivenMomentum().pz(),patObject.ecalDrivenMomentum().energy());
+}
+
+
+}
+
+DEFINE_EDM_PLUGIN(edm2pxlio::ConverterFactory, edm2pxlio::ElectronConverter, "ElectronConverter");
---
+++ Pat/plugins/ElectronConverter.h
@@ -0,0 +1,85 @@
+#ifndef __EDM2PXLIO_PAT_ELECTRONCONVERTER_H__
+#define __EDM2PXLIO_PAT_ELECTRONCONVERTER_H__
+
+// user include files
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/Common/interface/View.h"
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+
+#include "Pxl/Pxl/interface/pxl/core.hh"
+#include "Pxl/Pxl/interface/pxl/hep.hh"
+
+#include "EDM2PXLIO/Core/interface/CollectionClassConverter.h"
+
+
+
+
+
+
+#include "DataFormats/PatCandidates/interface/Electron.h"
+
+#include "DataFormats/VertexReco/interface/Vertex.h"
+
+//#include "EDM2PXLIO/EDM2PXLIO/src/provider/EffectiveAreaIsolationProvider.h"
+
+namespace edm2pxlio
+{
+
+class ElectronConverter: public CollectionClassConverter<pat::Electron>
+{
+    private:
+        typedef CollectionClassConverter<pat::Electron> Base;
+    protected:
+        //PrimaryVertexProvider* primaryVertexProvider_;
+        //EffectiveAreaIsolationProvider* effectiveAreaIsolationProvider_;
+
+    public:
+        ElectronConverter(const std::string& name, const edm::ParameterSet& globalConfig, edm::ConsumesCollector& consumesCollector);
+        
+        virtual void convertObject(const pat::Electron& patObject, pxl::Particle* pxlParticle) const;
+        
+        virtual void convertP4(const pat::Electron& patObject, pxl::Particle* pxlParticle) const;
+        /*
+        virtual void convertCollection(const edm::Handle<edm::View<pat::Electron>> patObjectList, std::vector<pxl::Particle*>& pxlParticleList)
+        {
+            CollectionClass2Pxlio<pat::Electron>::convertCollection(patObjectList, pxlParticleList);
+            
+            if (effectiveAreaIsolationProvider_->getEARho())
+            {
+                //besides reco eletrons, gsf electrons are also present in the map so the numbers do not match...
+                
+                //if (effectiveAreaIsolationProvider_->getEARho()->size()!=patObjectList->size())
+                //{
+                //   throw cms::Exception("PatElectron2Pxlio::convertCollection") << "electron ea03 value map differs in size compared to provided pat electrons";
+                // }
+                
+                for (unsigned iel=0; iel<patObjectList->size(); ++iel)
+                {
+                    const pat::Electron el = (*patObjectList)[iel];
+                    if (effectiveAreaIsolationProvider_->getEARho()->contains(el.originalObjectRef().id()))
+                    {
+                        double ea03 = (*effectiveAreaIsolationProvider_->getEARho())[el.originalObjectRef()];
+                        pxlParticleList[iel]->setUserRecord("ea03",ea03);
+                    }
+
+                }
+            }
+        }
+        */
+        virtual ~ElectronConverter()
+        {
+        }
+};
+
+}
+
+#endif
+
