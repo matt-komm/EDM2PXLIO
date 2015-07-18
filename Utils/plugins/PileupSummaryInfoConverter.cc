ce1be7377a221d071435771814195a59e5b43746
Matthias <mkomm@cern.ch>
2015-07-18 14:03:50 +0200

    adapted to run on data

--- Utils/plugins/PileupSummaryInfoConverter.cc
+++ Utils/plugins/PileupSummaryInfoConverter.cc
@@ -14,37 +14,43 @@ PileupSummaryInfoConverter::PileupSummaryInfoConverter(const std::string& name, 
 
 void PileupSummaryInfoConverter::convert(const edm::Event* edmEvent, const edm::EventSetup* iSetup, pxl::Event* pxlEvent) const
 {
+    //TODO: two producers: 1 for pileup summary, 1 for general reco::Vertex collection
     Base::convert(edmEvent, iSetup, pxlEvent);
+    pxl::EventView* pxlEventView = Base::findEventView(pxlEvent,Base::getEventViewName(0));
+    pxl::Particle* puObject = pxlEventView->create<pxl::Particle>();
+    puObject->setName(Base::getCollectionName(0));
 
     for (unsigned index=0; index<Base::size(); ++index)
     {
-        const edm::Handle<std::vector<PileupSummaryInfo>> collection = Base::getCollection(edmEvent,index);
-        pxl::EventView* pxlEventView = Base::findEventView(pxlEvent,Base::getEventViewName(index));
-        if (collection.product())
+        if (not edmEvent->isRealData())
         {
-            pxl::Particle* puObject = pxlEventView->create<pxl::Particle>();
-            puObject->setName(Base::getCollectionName(index));
+            const edm::Handle<std::vector<PileupSummaryInfo>> collection = Base::getCollection(edmEvent,index);
             
-            pxl::BasicNVector nInteractionVector(collection->size());
-            for (unsigned int i = 0; i < collection->size(); ++i)
+            if (collection.product())
             {
-                nInteractionVector.setElement(i,(*collection)[i].getPU_NumInteractions());
-                //puObject->setUserRecord(std::string("nInteractions_")+std::to_string(i),(*collection)[i].getPU_NumInteractions());
+                pxl::BasicNVector nInteractionVector(collection->size());
+                for (unsigned int i = 0; i < collection->size(); ++i)
+                {
+                    nInteractionVector.setElement(i,(*collection)[i].getPU_NumInteractions());
+                    //puObject->setUserRecord(std::string("nInteractions_")+std::to_string(i),(*collection)[i].getPU_NumInteractions());
+                }
+                puObject->setUserRecord("nInteractions"+std::to_string(index),nInteractionVector);
             }
-            puObject->setUserRecord("nInteractions",nInteractionVector);
-            if (_primaryVertexProvider->getPrimaryVertex())
-            {
-                puObject->setUserRecord("nVertices",_primaryVertexProvider->getNVertices());
-                puObject->setUserRecord("PVx",PRECISION(_primaryVertexProvider->getPrimaryVertex()->x()));
-                puObject->setUserRecord("PVy",PRECISION(_primaryVertexProvider->getPrimaryVertex()->y()));
-                puObject->setUserRecord("PVz",PRECISION(_primaryVertexProvider->getPrimaryVertex()->z()));
-                puObject->setUserRecord("PVxError",PRECISION(_primaryVertexProvider->getPrimaryVertex()->xError()));
-                puObject->setUserRecord("PVyError",PRECISION(_primaryVertexProvider->getPrimaryVertex()->yError()));
-                puObject->setUserRecord("PVzError",PRECISION(_primaryVertexProvider->getPrimaryVertex()->zError()));
-                
-                puObject->setUserRecord("PVchi",PRECISION(_primaryVertexProvider->getPrimaryVertex()->chi2()));
-                puObject->setUserRecord("PVndof",_primaryVertexProvider->getPrimaryVertex()->ndof());
-            }   puObject->setUserRecord("PVvalid",_primaryVertexProvider->getPrimaryVertex()->isValid());
+        }
+        
+        if (_primaryVertexProvider->getPrimaryVertex())
+        {
+            puObject->setUserRecord("nVertices",_primaryVertexProvider->getNVertices());
+            puObject->setUserRecord("PVx",PRECISION(_primaryVertexProvider->getPrimaryVertex()->x()));
+            puObject->setUserRecord("PVy",PRECISION(_primaryVertexProvider->getPrimaryVertex()->y()));
+            puObject->setUserRecord("PVz",PRECISION(_primaryVertexProvider->getPrimaryVertex()->z()));
+            puObject->setUserRecord("PVxError",PRECISION(_primaryVertexProvider->getPrimaryVertex()->xError()));
+            puObject->setUserRecord("PVyError",PRECISION(_primaryVertexProvider->getPrimaryVertex()->yError()));
+            puObject->setUserRecord("PVzError",PRECISION(_primaryVertexProvider->getPrimaryVertex()->zError()));
+            
+            puObject->setUserRecord("PVchi",PRECISION(_primaryVertexProvider->getPrimaryVertex()->chi2()));
+            puObject->setUserRecord("PVndof",_primaryVertexProvider->getPrimaryVertex()->ndof());
+            puObject->setUserRecord("PVvalid",_primaryVertexProvider->getPrimaryVertex()->isValid());
         }
     }
 }
--- stea.py
+++ stea.py
@@ -31,8 +31,6 @@ options.register(
 				  
 options.parseArguments()
 
-
-
 process = cms.Process("STEA")
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 process.options.allowUnscheduled = cms.untracked.bool(False)
@@ -100,24 +98,38 @@ process.btaggingSF = cms.EDProducer("BtagUncertainty",
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        'root://xrootd.unl.edu//store/mc/RunIISpring15DR74/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v1/50000/02CF0510-4CFF-E411-A715-0025905A6090.root'
-    )
+        #'root://xrootd.unl.edu//store/mc/RunIISpring15DR74/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v1/50000/02CF0510-4CFF-E411-A715-0025905A6090.root'
+        'root://xrootd.unl.edu//store/data/Run2015B/SingleMuon/MINIAOD/PromptReco-v1/000/251/244/00000/68275270-7C27-E511-B1F0-02163E011A46.root' #{golden run: 251244:96-251244:121}
+    ),
+    lumisToProcess = cms.untracked.VLuminosityBlockRange('251244:96-251244:121'),
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500) )
+
+
+process.STEA_plain=cms.Path()
+process.STEA_filtered=cms.Path()
+
+def addModule(m):
+    process.STEA_plain+=m
+    process.STEA_filtered+=m
 
 
 ### gen particle pruner ###
 
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
-process.lessGenParticles = cms.EDProducer("GenParticlePruner",
-    src=cms.InputTag("prunedGenParticles"),
-    select=cms.vstring(
-        "drop  *",
-        "++keep abs(status)>20 & abs(status) < 30", #keeps all particles from the hard matrix element and their mothers
-        "keep++ abs(pdgId)=24", #keep W-boson decay
-        "drop abs(status)>30", #drop all intermediate from decay
+
+if not options.isData:
+    process.lessGenParticles = cms.EDProducer("GenParticlePruner",
+        src=cms.InputTag("prunedGenParticles"),
+        select=cms.vstring(
+            "drop  *",
+            "++keep abs(status)>20 & abs(status) < 30", #keeps all particles from the hard matrix element and their mothers
+            "keep++ abs(pdgId)=24", #keep W-boson decay
+            "drop abs(status)>30", #drop all intermediate from decay
+        )
     )
-)
+    addModule(process.lessGenParticles)
+    
 
 
 
@@ -160,6 +172,7 @@ process.skimSequence = cms.Sequence(
     #*process.numPUPPIJetsFilter
 )
     
+process.STEA_filtered+=process.skimSequence
 
 
 ### electron IDs ###
@@ -172,6 +185,7 @@ for eleID in [
 ]:
     setupAllVIDIdsInModule(process,eleID,setupVIDElectronSelection)
 
+addModule(process.egmGsfElectronIDSequence)
 
 
 
@@ -271,132 +285,249 @@ filterPSet = cms.VPSet(
     )
 )
 
-process.pat2pxlio=cms.EDAnalyzer('EDM2PXLIO',
-    outFileName=cms.string("output.pxlio"),
-    processName=cms.string(options.processName),
-    selectEvents=cms.VPSet(
-        cms.PSet(
-            process=cms.string("STEA"),
-            paths=cms.vstring(
-                "STEA_plain","STEA_filtered"
-            ),
-        )
-    ),
-    primaryVertex = cms.InputTag("offlineSlimmedPrimaryVertices"),
 
-    muons = cms.PSet(
-        type=cms.string("MuonConverter"),
-        srcs=cms.VInputTag(cms.InputTag("slimmedMuons")),
-        names=cms.vstring("Muon"),
-        valueMaps = cms.PSet()
-    ),
-    
-    electrons = cms.PSet(
-        type=cms.string("ElectronConverter"),
-        srcs=cms.VInputTag(cms.InputTag("slimmedElectrons")),
-        names=cms.vstring("Electron"),
-        valueMaps=cms.PSet(
-            phys14eleIDVeto = cms.PSet(
-                type=cms.string("ValueMapAccessorBool"),
-                src=cms.InputTag("egmGsfElectronIDs","cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-veto")
-            ),
-            phys14eleIDTight = cms.PSet(
-                type=cms.string("ValueMapAccessorBool"),
-                src=cms.InputTag("egmGsfElectronIDs","cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-tight")
-            ),
-        )
-    ),
-                                 
-    jets = cms.PSet(
-        type=cms.string("JetConverter"),
-        srcs=cms.VInputTag(cms.InputTag("slimmedJets")),
-        names=cms.vstring("Jet"),
-        select=cms.string("pt>20.0"),
-        valueMaps=cms.PSet(
-            #jesUp = cms.PSet(
-            #    type=cms.string("ValueMapAccessorLorentzVector"),
-            #    src=cms.InputTag("jesUp","jets")
-            #),
-            #jesDown = cms.PSet(
-            #    type=cms.string("ValueMapAccessorLorentzVector"),
-            #    src=cms.InputTag("jesDown","jets")
-            #),
-            #btagSFB = cms.PSet(
-            #    type=cms.string("ValueMapAccessorFloat"),
-            #    src=cms.InputTag("btaggingSF","SFB")
-            #),
-            #btagSFBerr = cms.PSet(
-            #   type=cms.string("ValueMapAccessorFloat"),
-            #    src=cms.InputTag("btaggingSF","SFBerr")
-            #)
+if options.isData:
+    process.pat2pxlio=cms.EDAnalyzer('EDM2PXLIO',
+        outFileName=cms.string("output.pxlio"),
+        processName=cms.string(options.processName),
+        selectEvents=cms.VPSet(
+            cms.PSet(
+                process=cms.string("STEA"),
+                paths=cms.vstring(
+                    "STEA_plain",
+                    "STEA_filtered"
+                ),
+            )
         ),
-        triggerFilter=filterPSet
-    ),
-    
-    puppiJets = cms.PSet(
-        type=cms.string("JetConverter"),
-        srcs=cms.VInputTag(cms.InputTag("slimmedJetsPuppi")),
-        names=cms.vstring("PuppiJets"),
-        select=cms.string("pt>20.0"),
-        triggerFilter=filterPSet
-    ),
-    
-    genParticles= cms.PSet(
-        type=cms.string("GenParticleConverter"),
-        srcs=cms.VInputTag(cms.InputTag("lessGenParticles")),
-        targetEventViews=cms.vstring("Generated"),
-        #LHEEvent=cms.InputTag("source","","LHEFile"),
-        GenEventInfo=cms.InputTag("generator","","SIM")
-    ),
-    
-    genjets = cms.PSet(
-        type=cms.string("GenJetConverter"),
-        srcs=cms.VInputTag(cms.InputTag("slimmedGenJets")),
-        names=cms.vstring("GenJet"),
-        targetEventViews=cms.vstring("GenJets"),
-    ),
-                             
-    mets = cms.PSet(
-        type=cms.string("METConverter"),
-        srcs=cms.VInputTag(cms.InputTag("slimmedMETs")),
-        names=cms.vstring("MET"),
-        valueMaps=cms.PSet(
-            #jesUp = cms.PSet(
-            #    type=cms.string("ValueMapAccessorLorentzVector"),
-            #    src=cms.InputTag("jesUp","mets")
-            #),
-            #jesDown = cms.PSet(
-            #    type=cms.string("ValueMapAccessorLorentzVector"),
-            #    src=cms.InputTag("jesDown","mets")
-            #)
-        )
+        primaryVertex = cms.InputTag("offlineSlimmedPrimaryVertices"),
+
+        muons = cms.PSet(
+            type=cms.string("MuonConverter"),
+            srcs=cms.VInputTag(cms.InputTag("slimmedMuons")),
+            names=cms.vstring("Muon"),
+            valueMaps = cms.PSet()
+        ),
         
-    ),
-    puppiMets = cms.PSet(
-        type=cms.string("METConverter"),
-        srcs=cms.VInputTag(cms.InputTag("slimmedMETsPuppi")),
-        names=cms.vstring("PuppiMET"),
+        electrons = cms.PSet(
+            type=cms.string("ElectronConverter"),
+            srcs=cms.VInputTag(cms.InputTag("slimmedElectrons")),
+            names=cms.vstring("Electron"),
+            valueMaps=cms.PSet(
+                phys14eleIDVeto = cms.PSet(
+                    type=cms.string("ValueMapAccessorBool"),
+                    src=cms.InputTag("egmGsfElectronIDs","cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-veto")
+                ),
+                phys14eleIDTight = cms.PSet(
+                    type=cms.string("ValueMapAccessorBool"),
+                    src=cms.InputTag("egmGsfElectronIDs","cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-tight")
+                ),
+            )
+        ),
+                                     
+        jets = cms.PSet(
+            type=cms.string("JetConverter"),
+            srcs=cms.VInputTag(cms.InputTag("slimmedJets")),
+            names=cms.vstring("Jet"),
+            select=cms.string("pt>20.0"),
+            valueMaps=cms.PSet(
+                #jesUp = cms.PSet(
+                #    type=cms.string("ValueMapAccessorLorentzVector"),
+                #    src=cms.InputTag("jesUp","jets")
+                #),
+                #jesDown = cms.PSet(
+                #    type=cms.string("ValueMapAccessorLorentzVector"),
+                #    src=cms.InputTag("jesDown","jets")
+                #),
+                #btagSFB = cms.PSet(
+                #    type=cms.string("ValueMapAccessorFloat"),
+                #    src=cms.InputTag("btaggingSF","SFB")
+                #),
+                #btagSFBerr = cms.PSet(
+                #   type=cms.string("ValueMapAccessorFloat"),
+                #    src=cms.InputTag("btaggingSF","SFBerr")
+                #)
+            ),
+            triggerFilter=filterPSet
+        ),
         
-    ),
-    
-    triggersHLT = cms.PSet(
-        type=cms.string("TriggerResultConverter"),
-        srcs=cms.VInputTag(cms.InputTag("TriggerResults","","HLT")),
-        regex=cms.vstring("HLT_Iso[0-9a-zA-z]*","HLT_Ele[0-9a-zA-z]*")
-    ),
-    
-    triggersPAT = cms.PSet(
-        type=cms.string("TriggerResultConverter"),
-        srcs=cms.VInputTag(cms.InputTag("TriggerResults","","PAT")),
-        regex=cms.vstring("[0-9a-zA-z]*")
-    ),
-    
-    puInfo = cms.PSet(
-        type=cms.string("PileupSummaryInfoConverter"),
-        srcs=cms.VInputTag(cms.InputTag("addPileupInfo","","HLT")),
-        names=cms.vstring("PU")
-    ),
-)
+        puppiJets = cms.PSet(
+            type=cms.string("JetConverter"),
+            srcs=cms.VInputTag(cms.InputTag("slimmedJetsPuppi")),
+            names=cms.vstring("PuppiJets"),
+            select=cms.string("pt>20.0"),
+            triggerFilter=filterPSet
+        ),
+                                 
+        mets = cms.PSet(
+            type=cms.string("METConverter"),
+            srcs=cms.VInputTag(cms.InputTag("slimmedMETs")),
+            names=cms.vstring("MET"),
+            valueMaps=cms.PSet(
+                #jesUp = cms.PSet(
+                #    type=cms.string("ValueMapAccessorLorentzVector"),
+                #    src=cms.InputTag("jesUp","mets")
+                #),
+                #jesDown = cms.PSet(
+                #    type=cms.string("ValueMapAccessorLorentzVector"),
+                #    src=cms.InputTag("jesDown","mets")
+                #)
+            )
+            
+        ),
+        puppiMets = cms.PSet(
+            type=cms.string("METConverter"),
+            srcs=cms.VInputTag(cms.InputTag("slimmedMETsPuppi")),
+            names=cms.vstring("PuppiMET"),
+            
+        ),
+        
+        triggersHLT = cms.PSet(
+            type=cms.string("TriggerResultConverter"),
+            srcs=cms.VInputTag(cms.InputTag("TriggerResults","","HLT")),
+            regex=cms.vstring("HLT_Iso[0-9a-zA-z]*","HLT_Ele[0-9a-zA-z]*")
+        ),
+        triggersRECO = cms.PSet(
+            type=cms.string("TriggerResultConverter"),
+            srcs=cms.VInputTag(cms.InputTag("TriggerResults","","RECO")),
+            regex=cms.vstring("[0-9a-zA-z]*")
+        ),
+        
+        
+        puInfo = cms.PSet(
+            type=cms.string("PileupSummaryInfoConverter"),
+            srcs=cms.VInputTag(cms.InputTag("addPileupInfo","","HLT")),
+            names=cms.vstring("PU")
+        ),
+    )
+
+
+else:
+    process.pat2pxlio=cms.EDAnalyzer('EDM2PXLIO',
+        outFileName=cms.string("output.pxlio"),
+        processName=cms.string(options.processName),
+        selectEvents=cms.VPSet(
+            cms.PSet(
+                process=cms.string("STEA"),
+                paths=cms.vstring(
+                    "STEA_plain","STEA_filtered"
+                ),
+            )
+        ),
+        primaryVertex = cms.InputTag("offlineSlimmedPrimaryVertices"),
+
+        muons = cms.PSet(
+            type=cms.string("MuonConverter"),
+            srcs=cms.VInputTag(cms.InputTag("slimmedMuons")),
+            names=cms.vstring("Muon"),
+            valueMaps = cms.PSet()
+        ),
+        
+        electrons = cms.PSet(
+            type=cms.string("ElectronConverter"),
+            srcs=cms.VInputTag(cms.InputTag("slimmedElectrons")),
+            names=cms.vstring("Electron"),
+            valueMaps=cms.PSet(
+                phys14eleIDVeto = cms.PSet(
+                    type=cms.string("ValueMapAccessorBool"),
+                    src=cms.InputTag("egmGsfElectronIDs","cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-veto")
+                ),
+                phys14eleIDTight = cms.PSet(
+                    type=cms.string("ValueMapAccessorBool"),
+                    src=cms.InputTag("egmGsfElectronIDs","cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-tight")
+                ),
+            )
+        ),
+                                     
+        jets = cms.PSet(
+            type=cms.string("JetConverter"),
+            srcs=cms.VInputTag(cms.InputTag("slimmedJets")),
+            names=cms.vstring("Jet"),
+            select=cms.string("pt>20.0"),
+            valueMaps=cms.PSet(
+                #jesUp = cms.PSet(
+                #    type=cms.string("ValueMapAccessorLorentzVector"),
+                #    src=cms.InputTag("jesUp","jets")
+                #),
+                #jesDown = cms.PSet(
+                #    type=cms.string("ValueMapAccessorLorentzVector"),
+                #    src=cms.InputTag("jesDown","jets")
+                #),
+                #btagSFB = cms.PSet(
+                #    type=cms.string("ValueMapAccessorFloat"),
+                #    src=cms.InputTag("btaggingSF","SFB")
+                #),
+                #btagSFBerr = cms.PSet(
+                #   type=cms.string("ValueMapAccessorFloat"),
+                #    src=cms.InputTag("btaggingSF","SFBerr")
+                #)
+            ),
+            triggerFilter=filterPSet
+        ),
+        
+        puppiJets = cms.PSet(
+            type=cms.string("JetConverter"),
+            srcs=cms.VInputTag(cms.InputTag("slimmedJetsPuppi")),
+            names=cms.vstring("PuppiJets"),
+            select=cms.string("pt>20.0"),
+            triggerFilter=filterPSet
+        ),
+        
+        genParticles= cms.PSet(
+            type=cms.string("GenParticleConverter"),
+            srcs=cms.VInputTag(cms.InputTag("lessGenParticles")),
+            targetEventViews=cms.vstring("Generated"),
+            #LHEEvent=cms.InputTag("source","","LHEFile"),
+            GenEventInfo=cms.InputTag("generator","","SIM")
+        ),
+        
+        genjets = cms.PSet(
+            type=cms.string("GenJetConverter"),
+            srcs=cms.VInputTag(cms.InputTag("slimmedGenJets")),
+            names=cms.vstring("GenJet"),
+            targetEventViews=cms.vstring("GenJets"),
+        ),
+                                 
+        mets = cms.PSet(
+            type=cms.string("METConverter"),
+            srcs=cms.VInputTag(cms.InputTag("slimmedMETs")),
+            names=cms.vstring("MET"),
+            valueMaps=cms.PSet(
+                #jesUp = cms.PSet(
+                #    type=cms.string("ValueMapAccessorLorentzVector"),
+                #    src=cms.InputTag("jesUp","mets")
+                #),
+                #jesDown = cms.PSet(
+                #    type=cms.string("ValueMapAccessorLorentzVector"),
+                #    src=cms.InputTag("jesDown","mets")
+                #)
+            )
+            
+        ),
+        puppiMets = cms.PSet(
+            type=cms.string("METConverter"),
+            srcs=cms.VInputTag(cms.InputTag("slimmedMETsPuppi")),
+            names=cms.vstring("PuppiMET"),
+            
+        ),
+        
+        triggersHLT = cms.PSet(
+            type=cms.string("TriggerResultConverter"),
+            srcs=cms.VInputTag(cms.InputTag("TriggerResults","","HLT")),
+            regex=cms.vstring("HLT_Iso[0-9a-zA-z]*","HLT_Ele[0-9a-zA-z]*")
+        ),
+        
+        triggersPAT = cms.PSet(
+            type=cms.string("TriggerResultConverter"),
+            srcs=cms.VInputTag(cms.InputTag("TriggerResults","","PAT")),
+            regex=cms.vstring("[0-9a-zA-z]*")
+        ),
+        
+        puInfo = cms.PSet(
+            type=cms.string("PileupSummaryInfoConverter"),
+            srcs=cms.VInputTag(cms.InputTag("addPileupInfo","","HLT")),
+            names=cms.vstring("PU")
+        ),
+    )
 
 '''
 for puppiIsoMuon in puppiIsoMuonList:
@@ -415,38 +546,15 @@ for puppiIsoElectron in puppiIsoElectronList:
 '''
     
     
-process.STEA_plain=cms.Path(
-    process.lessGenParticles
-    *process.egmGsfElectronIDSequence
-    #*process.jesUp
-    #*process.jesDown
-    #*process.btaggingSF
-    #*process.PFSequence
-    #*process.pfDeltaBetaWeightingSequence
-    #*process.puppiSequence
-    #*process.pfWeightedLeptonIso
-)
 
-process.STEA_filtered=cms.Path(
-    process.skimSequence
-    *process.lessGenParticles
-    *process.egmGsfElectronIDSequence
-    #*process.jesUp
-    #*process.jesDown
-    #*process.btaggingSF
-    #*process.PFSequence
-    #*process.pfDeltaBetaWeightingSequence
-    #*process.puppiSequence
-    #*process.pfWeightedLeptonIso
-)
 
 process.endpath= cms.EndPath()
 
 process.endpath+=process.pat2pxlio
-'''
+
 process.OUT = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('output.root'),
-    outputCommands = cms.untracked.vstring('keep *','keep patJets_patJetsAK4PF_*_*','keep patJets_patJetsAK4PFCHS_*_*','keep *_*_*_PAT','keep *_*_*_S2')
+    outputCommands = cms.untracked.vstring('keep *')
 )
 process.endpath+= process.OUT
-'''
+
